c0e16949ab383c228ec53a1303b2776002182dbb
Nagaraj Murugesan <nagaraj_murugesan@persistent.com>
2024-01-05 11:46:41 +0530

    first-commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "hello",
+        component: HelloComponent
+    },
+    {
+        path : "home",
+        component: HomeComponent
+    },
+    {
+        path : "",
+        component:HelloComponent,
+         
+    }
+];
